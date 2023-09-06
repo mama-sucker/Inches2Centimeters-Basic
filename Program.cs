f4fe1abf1c4f06df3fb0b798b13f94cc68d2cbc3
Addison Roy <74374312+mama-sucker@users.noreply.github.com>
2023-09-06 11:06:38 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Module 2: Excercise 2.1 
+// Inches to Centimeters 
+// Addison Roy
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        double inches = 15994;
+        double centimeters = inches * 2.54;
+        Console.WriteLine($"Inches = {inches}");
+        Console.WriteLine($"centimeters = {centimeters}");
+    }
+}
